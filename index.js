ab9a198a243f3596a8adfe6f4c7f82bea132aa1d
MayurChawla <mr.mayurchawla@gmail.com>
2021-10-07 06:45:08 +0530

    adding js

---
+++ index.js
@@ -0,0 +1,14 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#next-button");
+const nextButton = document.querySelector("#next-button");
+const checkButton = document.querySelector("#check-button");
+const errorMessage = document.querySelector("#error-message");
+nextButton.addEventListener("click", function validateBillAmount(){
+    if(billAmount.value>0)
+    {
+        
+    }
+    else {
+
+    }
+});
