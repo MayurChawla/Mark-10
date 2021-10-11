2226eaf12f663943b210e8e8807e93bcc0ef1e48
MayurChawla <mr.mayurchawla@gmail.com>
2021-10-11 09:19:11 +0530

    logic done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cashGiven = document.querySelector("#cash-given");
 const nextButton = document.querySelector("#next-button");
 const checkButton = document.querySelector("#check-button");
 const errorMessage = document.querySelector("#error-message");
-const noOfNotes = document.querySelector(".no-of-notes");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000,500,100,20,10,5,1];
 
@@ -12,7 +12,7 @@ nextButton.addEventListener("click", function validateBillAmount(){
     if(billAmount.value>0)
     {
         if(cashGiven.value>= billAmount.value) {
-            const amountToBeReturned = cashGiven - billAmount;
+            const amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
         }
         else {
@@ -31,7 +31,6 @@ function hideMessage(){
     errorMessage.style.display = "none";
 }
 function calculateChange(returnAmount){
-    console.log(noOfNotes);
     for(let i=0;i<availableNotes.length;i++)
     {
         const numberofNotes = Math.trunc (
